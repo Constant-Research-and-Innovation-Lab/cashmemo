c7d22e1d0ca68ddffc26d13d67ead16a0054b30e
mdabdurrahman07 <jamilrahman2001@gmail.com>
2024-10-28 01:02:06 +0600

    cloudnary User display pic and name setup and dashbaord layout setup

--- frontend/src/routers/AppRouter.jsx
+++ frontend/src/routers/AppRouter.jsx
@@ -2,25 +2,26 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import PrivateRoute from "./PrivateRoute";
 import Secret from "../shared/Secret";
+import DashBoard from "../DashBoard/DashBoard";
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: "",
     element: <App />,
     children: [
       {
-        path: "/",
+        path: "",
         element: <h1>Home</h1>,
       },
       {
-        path: "/orders",
+        path: "orders",
         element: <div> Orders </div>,
       },
       {
-        path: "/about",
+        path: "about",
         element: <div> About </div>,
       },
       {
-        path: "/secret",
+        path: "secret",
         element: (
           <PrivateRoute>
             <Secret />
@@ -29,6 +30,20 @@ const router = createBrowserRouter([
       },
     ],
   },
+  {
+    path:"dashboard",
+    element: <DashBoard/>,
+    children: [
+      {
+        path: "",
+        element : <div>Dashboard</div>
+      },
+      {
+        path: "profile",
+        element : <div>Profile</div>
+      }
+    ]
+  }
 ]);
 
 export default router;
